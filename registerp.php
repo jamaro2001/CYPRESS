61e5d9d02b645b63067c6f2b5a6af22c86ea0d49
jamaro2001 <69558592+jamaro2001@users.noreply.github.com>
2021-04-13 19:14:54 -0400

    Update registerp.php

--- registerp.php
+++ registerp.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CYPRESS - Registration </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
 </head>
@@ -44,6 +45,14 @@
 </script>
 
 <body>
+  <header>
+    <div class="flex-container" style="justify-content:space-between;">
+       <h3>CYPRESS</h3>
+       <h3>City of Toronto</h3>
+    </div>
+    <hr>
+  </header>
+    
     <div class="row">
         <div class="col-12 text-center">
              <h1 class="mainHeads">CYPRESS Registration</h1>
