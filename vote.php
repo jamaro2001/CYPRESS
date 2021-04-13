8721b0d9a1fc13d92859c7e2b48a818388c31c91
Inthuyen Naguleswaran <79668965+inthuyen@users.noreply.github.com>
2021-04-13 19:32:04 -0400

    Cypress vote page
    User can vote on problems that they have experienced.

---
+++ vote.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CYPRESS - Vote</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+  <header>
+    <div class="flex-container" style="justify-content:space-between;">
+       <h3>CYPRESS</h3>
+       <h3>City of Toronto</h3>
+    </div>
+    <hr>
+  </header>
+    
+    <div class="row">
+        <div class="col-12 text-center">
+             <h1 class="mainHeads">CYPRESS Vote</h1>
+        </div>
+    </div>
+    
+    <div class="container">
+        <h6>
+            You are currently at the Cypress voting page. Please select all of the problems you are experiencing below.
+        </h6>
+    </div>
+    
+    <div class="container">
+        <div class="col-xs">
+        <form action="" method="post" name="login_form">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Problem 1</label>
+            </div>
+            
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Problem 2</label>
+            </div>
+            
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Problem 3</label>
+            </div>
+            
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Problem 4</label>
+            </div>
+            
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Problem 5</label>
+            </div>
+  
+            <button type="submit" class="btn btn-danger">Submit</button>
+        </form> 
+        </div>
+    </div>
+</body>
+</html>
