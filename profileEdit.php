1b8223fa1a690b4b015f658518d148b545793ce0
jamaro2001 <69558592+jamaro2001@users.noreply.github.com>
2021-04-13 20:37:47 -0400

    Update profileEdit.php

--- profileEdit.php
+++ profileEdit.php
@@ -17,8 +17,8 @@ if(!isset($_SESSION['username'])){
   <title>CYPRESS - Edit Information</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <script>
 // check if input values are good and legal
