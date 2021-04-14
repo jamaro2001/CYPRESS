00b8d8266893abbd77fd8769e9cdc22071d68f6d
jamaro2001 <69558592+jamaro2001@users.noreply.github.com>
2021-04-14 18:54:06 -0400

    Update and rename allReports.html to allReports.php

---
+++ allReports.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<?php
+  $reports = file('reports.txt', FILE_IGNORE_NEW_LINES);
+?>
+  
+<head>
+  <meta charset=“UTF-8”>
+  <meta name=“viewport” content=“width=device-width, initial-scale=1.0”>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>View Reports</title>
+</head>
+
+<body>
+  <header>
+    <div class="flex-container" style="justify-content:space-between;">
+       <h3>CYPRESS</h3>
+       <h3>City of Toronto</h3>
+    </div>
+    <hr>
+  </header>
+  
+  <div class="center">
+    <h4>All Currently Unresolved Reports</h4>
+    
+    <!--- Code to retrieve and display reports --->
+    <?php
+      for ($x = 0; $x = count($reports); $++ 2)
+      {
+        echo "<div class='flex-container' style='justify-content:center'>";
+        echo "<p>". $reports[$x] ."</p>";
+        echo "<button type='button'><a href='edit_report.html'>Edit</a></button>";
+        echo "<button type='button'> <a href='delete.php'>Delete</a> </button>";
+        echo "</div>";
+      }
+    ?>
+    
+  </div>
+</body>
+
+</html>
