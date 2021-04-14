b2d815b8027364a783a37f1626974f7120805de5
jamaro2001 <69558592+jamaro2001@users.noreply.github.com>
2021-04-14 19:27:12 -0400

    Update vote.php

--- vote.php
+++ vote.php
@@ -7,6 +7,11 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
+<?php
+  $reports = file('reports.txt', FILE_IGNORE_NEW_LINES);
+?>
+	
 <body>
   <header>
     <div class="flex-container" style="justify-content:space-between;">
@@ -31,20 +36,13 @@
     <div class="container">
         <div class="col-xs">
         <form action="" method="post" name="login_form">
-                <input type="checkbox"id="customCheck1" name="customCheck1" value="1">
-                <label class="custom-control-label" for="customCheck1">86 Steeles Ave. West, City property vandalism</label> <br>
-            
-                <input type="checkbox"id="customCheck2" name="customCheck2" value="2">
-                <label class="custom-control-label" for="customCheck2">136 Hilda Ave., Potholes</label> <br>
-            
-                <input type="checkbox"id="customCheck3" name="customCheck3" value="3">
-                <label class="custom-control-label" for="customCheck3">8 Royal Oak Dr., Tree collapse</label> <br>
-				
-                <input type="checkbox"id="customCheck4" name="customCheck4" value="4"> 
-                <label class="custom-control-label" for="customCheck4">6 Eglinton Ave. West, Garbage or any other road-blocking objects</label> <br>
-				
-                <input type="checkbox"id="customCheck5" name="customCheck5" value="5">
-                <label class="custom-control-label" for="customCheck5">2190 Yonge St., City property vandalism</label> <br>
+		<?php
+      		for ($x = 0; $x < count($reports); $x++)
+      		{
+       			echo '<input type="checkbox"id="customCheck'.$x.'" name="customCheck'.$x.'" value="'.$x.'">';
+                	echo '<label class="custom-control-label" for="customCheck'.$x.'">'. $reports[$x] .'</label> <br>';
+    		}
+   		?>
             <button type="submit" class="btn btn-danger"> <a href="vote_confirmation.php">Submit</a> </button>
         </form> 
         </div>
