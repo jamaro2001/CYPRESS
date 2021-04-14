ce9267e2a7ee6748d436ad1bbe04261a7ba98a68
inthuyen <79668965+inthuyen@users.noreply.github.com>
2021-04-13 23:22:26 -0400

    Create vote_confirmation.php

---
+++ vote_confirmation.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CYPRESS - Survey</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <header>
+    <div class="flex-container" style="justify-content:space-between;">
+       <h3>CYPRESS</h3>
+       <h3>City of Toronto</h3>
+    </div>
+    <hr>
+  </header>
+
+    <div class="row">
+         <div class="col-12 text-center">
+             <h4 class="mainHeads">Thank you for voting!</h>
+        </div>
+    </div>
+    
+    <button type="submit" class="btn btn-danger"> <a href="portal.html">Return Home</a> </button>
+    
+</body>
+</html>
+    
